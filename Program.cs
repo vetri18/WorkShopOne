e4b2e708de01c74065b297fe157a512864886f9b
vetrivel <vetrivel.kce@gmail.com>
2022-09-21 22:30:49 +0530

    welcome to summation of digits problem

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace WorkShopOne
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Summation Of Digits ");
+        }
+    }
+}
