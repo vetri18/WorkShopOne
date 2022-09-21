ea896889ef8d7b7309b0068615c51d75ad317659
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-21 22:33:01 +0530

    Summation of digits

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-namespace WorkShopOne
+﻿namespace Workshop
 {
-    class Program
+    class program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Summation Of Digits ");
+            Console.WriteLine("enter the number : ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = number.ToString().Select(digit => int.Parse(digit.ToString())).ToArray().Sum();
+            Console.WriteLine($"THE SUM OF DIGIT IS : {sum} ");
+            Console.ReadKey();
         }
     }
 }
